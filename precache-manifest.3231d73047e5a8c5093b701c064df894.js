928e34ad1cd0c113eab8060826607a16ef7ab894
Emir Bojagic <emir.bojagic@serapion.net>
2019-01-28 09:14:24 +0100

    Updates

---
+++ precache-manifest.3231d73047e5a8c5093b701c064df894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57bcd3e1d6bbe6c398db",
+    "url": "/static/js/main.57bcd3e1.chunk.js"
+  },
+  {
+    "revision": "54252f1a25fc928b3cc2",
+    "url": "/static/js/1.54252f1a.chunk.js"
+  },
+  {
+    "revision": "57bcd3e1d6bbe6c398db",
+    "url": "/static/css/main.443ecfcb.chunk.css"
+  },
+  {
+    "revision": "443ddc24198f33309b9f735aeea209da",
+    "url": "/index.html"
+  }
+];
